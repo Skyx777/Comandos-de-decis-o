0e3f405ea12780047fded2bbbe450c21937ed7a0
Skyx777 <113266792+Skyx777@users.noreply.github.com>
2022-11-28 10:06:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let numero = prompt("Digite o valor da compra : ")
+if(numero>50){
+    alert('Compra não aprovada')
+}
+else if (numero<50){
+    alert('Compra aprovada')
+}
+else{
+    alert('Compra aprovada')
+}
+
+
+
+
+
+
+
